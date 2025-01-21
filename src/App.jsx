583f79a8be3fe399acf0dd7d758355e66ec63a60
myigitcelik <myigitcelik@yahoo.com>
2025-01-21 15:20:41 +0100

    -viteconfig fix

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
   return (
     <BrowserRouter basename="/">
     <Navbar></Navbar>
-
       <Routes>
         <Route path="/" element={<Homepage></Homepage>}></Route>
         <Route path="/contact" element={<Contact></Contact>}></Route>
